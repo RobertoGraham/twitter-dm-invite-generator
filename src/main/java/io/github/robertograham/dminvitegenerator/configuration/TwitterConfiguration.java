6fa04b6d2e3741be1f060e535d96276e898f33a6
unknown <robertogrhm@gmail.com>
2019-01-16 18:10:52 +0000

    Added Twitter bean and heroku config files

---
+++ src/main/java/io/github/robertograham/dminvitegenerator/configuration/TwitterConfiguration.java
@@ -0,0 +1,28 @@
+package io.github.robertograham.dminvitegenerator.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+@Configuration
+public class TwitterConfiguration {
+
+    @Bean
+    public Twitter twitter(@Value("twitter.consumerKey") final String consumerKey,
+                           @Value("twitter.consumerSecret") final String consumerSecret,
+                           @Value("twitter.accessToken") final String accessToken,
+                           @Value("twitter.accessTokenSecret") final String accessTokenSecret) {
+        return new TwitterFactory(
+            new ConfigurationBuilder()
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret)
+                .build()
+        )
+            .getInstance();
+    }
+}
